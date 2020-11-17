f4e42d59a8f5ad3a08425760cd7b0891cb061443
xiaoz <xiaoz93@outlook.com>
2020-11-17 14:39:46 +0800

    修复BUG

--- run.sh
+++ run.sh
@@ -2,17 +2,17 @@
 
 
 #自动重载
-if [ $1 == 'autoreload' ]
+if [ ${1}x == 'autoreload'x ]
 	then
 		find /usr/local/nginx/conf/cdn -mmin 1 -exec "/usr/local/nginx/sbin/nginx -t && /usr/local/nginx/sbin/nginx" -s reload {} \;
 		find /usr/local/nginx/conf/vhost -mmin 1 -exec "/usr/local/nginx/sbin/nginx -t && /usr/local/nginx/sbin/nginx" -s reload {} \;
-	elif [ $1 == 'reload' ]
+	elif [ ${1}x == 'reload'x ]
 	then
 		/usr/local/nginx/sbin/nginx -t && /usr/local/nginx/sbin/nginx -s reload
-	elif [ $1 == 'stop' ]
+	elif [ ${1}x == 'stop'x ]
 	then
 		/usr/local/nginx/sbin/nginx -t && /usr/local/nginx/sbin/nginx -s stop
-	elif [ $1 == 'start' ]
+	elif [ ${1}x == 'start'x ]
 	then
 		/usr/local/nginx/sbin/nginx
 	elif [ -z $1 ]
